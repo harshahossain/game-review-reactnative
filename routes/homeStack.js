2cda3fa31ac09384ac6410643e50ecc608be8ccf
harshahossain <harshahossain@gmail.com>
2024-02-19 23:40:13 +0600

    updated

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -7,12 +7,37 @@ import About from "../screens/about";
 const Stack = createNativeStackNavigator();
 
 export default function Navigator() {
+  const headerStyles = {
+    backgroundColor: "#CDD6D1",
+    headerTintColor: "#444",
+  };
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Details" component={ReviewDetails} />
-        <Stack.Screen name="About" component={About} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: "Game Review",
+            headerStyle: headerStyles,
+          }}
+        />
+        <Stack.Screen
+          name="Details"
+          component={ReviewDetails}
+          options={{
+            title: "Review Details",
+            headerStyle: headerStyles,
+          }}
+        />
+        <Stack.Screen
+          name="About"
+          component={About}
+          options={{
+            title: "About Us",
+            headerStyle: headerStyles,
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
